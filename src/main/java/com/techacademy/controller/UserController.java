1ace5ee94919b194737078e993f8f14d580b7809
takumi-sashiki <tkm.ssk.wilson.1211@gmail.com>
2024-02-02 16:24:55 +0900

    2024/2/2 提出課題

---
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.techacademy.controller;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techacademy.entity.User;
+import com.techacademy.service.UserService;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    /** 一覧画面を表示 */
+    @GetMapping("/list")
+    public String getList(Model model) {
+        model.addAttribute("userlist", service.getUserList());
+        return "user/list";
+    }
+
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        return "/user/register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            return getRegister(user);
+        }
+        service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/update/{id}/")
+    public String getUser(@ModelAttribute User user, @PathVariable("id") Integer id, Model model) {
+        if (id != null) {
+            model.addAttribute("user", service.getUser(id));
+        } else {
+            model.addAttribute("user", user);
+        }
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}/")
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            return getUser(user, null, model);
+        }
+        service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @PostMapping(path = "list", params = "deleteRun")
+    public String deleteRun(@RequestParam(name = "idck") Set<Integer> idck, Model model) {
+        service.deleteUser(idck);
+        return "redirect:/user/list";
+    }
+}
---
+++ src/main/java/com/techacademy/entity/Authentication.java
@@ -0,0 +1,24 @@
+package com.techacademy.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "authentication")
+public class Authentication {
+    @Id
+    private String loginUser;
+
+    private String password;
+
+    private String validDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
